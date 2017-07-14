718dd114364c7ce5e0406a18adee715948a88b31
NajibOsman <machidydemajka38@gmail.com>
2017-07-14 18:38:46 +0300

    Included the code to pass the spec in client.rb

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,4 +1,4 @@
-class List
+class Stylist
   attr_reader(:name, :id)
 
   define_method(:initialize) do |attributes|
@@ -12,9 +12,25 @@ class List
     returned_stylists.each do |stylist|
       name = stylist.fetch('name')
       id = stylist.fetch('id').to_i
-      stylists.push(Stylists.new(:name name, id: id))
+      stylists.push(Stylist.new(name: name, id: id))
     end
     stylists
   end
 
-  
+  define_method(:save) do
+    result = DB.exec("INSERT INTO stylists (name) VALUES ('#{@name}') RETURNING id;")
+    @id = result.first.fetch('id').to_i
+  end
+
+  define_singleton_method(:find) do |id|
+    found_stylist = nil
+    Stylist.all.each do |stylist|
+      found_stylist = stylist if stylist.id.==(id)
+    end
+    found_stylist
+  end
+
+  define_method(:==) do |another_stylist|
+    name.==(another_stylist.name).&(id.==(another_stylist.id))
+  end
+end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -18,7 +18,76 @@ describe(Stylist) do
     it('sets its ID when you save it') do
       stylist = Stylist.new(name: 'Najib', id: nil)
       stylist.save
-      expect(stylist.id).to(be_an_instance_of(Integer))
+      expect(stylist.id).to(be_an_instance_of(Fixnum))
     end
   end
+
+  describe('#save') do
+    it('lets you save stylist to the database') do
+      stylist = Stylist.new(name: 'Najib', id: nil)
+      stylist.save
+      expect(Stylist.all).to(eq([stylist]))
+    end
+  end
+
+  describe('.find') do
+    it('returns a stylist by its ID') do
+      test_stylist = Stylist.new(name: 'Najib', id: nil)
+      test_stylist.save
+      test_stylist2 = Stylist.new(name: 'Osman', id: nil)
+      test_stylist2.save
+      expect(Stylist.find(test_stylist2.id)).to(eq([test_stylist2]))
+    end
+  end
+
+  describe('#==') do
+    it('is the same stylist is it has the same name') do
+      stylist1 = Stylist.new(name: 'Najib', id: nil)
+      stylist2 = Stylist.new(name: 'Najib', id: nil)
+      expect(stylist1).to(eq(stylist2))
+    end
+  end
+  describe('#clients') do
+    it('returns an array of clients for that stylist') do
+      test_stylist = Stylist.new(name: 'Najib', id: nil)
+      test_stylist.save
+      test_client = Client.new(description: 'Learn SQL', list_id: test_list.id)
+      test_client.save
+      test_client2 = Client.new(description: 'Review Ruby', list_id: test_list.id)
+      test_client2.save
+      expect(test_stylist.clients).to(eq([test_client, test_client2]))
+    end
+  end
+  describe('#update') do
+    it('lets you update stylists in the database') do
+      stylist = Stylist.new(name: 'Najib', id: nil)
+      stylist.save
+      stylist.update(name: 'Osman')
+      expect(stylist.name).to(eq('Osman'))
+    end
+  end
+
+  describe('#delete') do
+    it('lets you delete a stylist from the database') do
+      stylist = Stylist.new(name: 'Najib', id: nil)
+      stylist.save
+      stylist2 = Stylist.new(name: 'Osman', id: nil)
+      stylist2.save
+      stylist.delete
+      expect(Stylist.all).to(eq([stylist2]))
+    end
+
+    it("deletes a stylist's tasks from the database") do
+      stylist = Stylist.new(name: 'Najib', id: nil)
+      stylist.save
+      client = Client.new(description: 'learn SQL', stylist_id: stylist.id)
+      client.save
+      client2 = Client.new(description: 'Review Ruby', stylist_id: stylist.id)
+      client2.save
+      stylist.delete
+      expect(Client.all).to(eq([]))
+    end
+  end
+
+
 end
